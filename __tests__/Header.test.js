501cc8254504bfcd456c93f8a201b560a8434484
Gastón Ferreyra <gferreyra91@gmail.com>
2021-05-31 22:16:47 -0300

    added header test

---
+++ __tests__/Header.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Header from '../src/components/Header';
+jest.mock('react-native-linear-gradient', () => 'LinearGradient');
+describe('<Header />', () => {
+  test('render header with title and subtitle', () => {
+    const PROPS = {
+      title: 'Test title',
+      subtitle:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin luctus ipsum nec sapien pretium iaculis. Vestibulum condimentum sapien vitae felis consequat maximus.',
+    };
+    const wrapper = renderer.create(<Header {...PROPS} />);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  test('render header with title only', () => {
+    const PROPS = {
+      title: 'Test title',
+    };
+    const wrapper = renderer.create(<Header {...PROPS} />);
+    expect(wrapper).toMatchSnapshot();
+  });
+});
